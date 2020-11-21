afaf87c623806d95a1caf9a1decb48a8c8a1633d
osyrisrblx <osyrisrblx@gmail.com>
2020-11-21 01:03:57 -0800

    Remove old Symbol types

--- types/Symbol.d.ts
+++ types/Symbol.d.ts
@@ -1,38 +1,9 @@
 /// <reference no-default-lib="true"/>
 
-interface Symbol {
-	/**
-	 * Returns a string representation of this data structure.
-	 */
-	toString(this: Symbol): string;
-}
+interface Symbol {}
 
 interface SymbolConstructor {
-	/**
-	 * A method that returns the default iterator for an object. Called by the semantics of the
-	 * for-of statement.
-	 */
 	readonly iterator: symbol;
 	readonly asyncIterator: symbol;
-
-	/**
-	 * Returns a new unique Symbol value.
-	 * @param  description Description of the new Symbol object.
-	 */
-	(description?: string | number): symbol;
-
-	/**
-	 * Returns a Symbol object from the global symbol registry matching the given key if found.
-	 * Otherwise, returns a new symbol with this key.
-	 * @param key key to search for.
-	 */
-	for(this: SymbolConstructor, key: string): symbol;
-
-	/**
-	 * Returns a key from the global symbol registry matching the given Symbol if found.
-	 * Otherwise, returns a undefined.
-	 * @param sym Symbol to find the key for.
-	 */
-	keyFor(this: SymbolConstructor, sym: symbol): string | undefined;
 }
 declare const Symbol: SymbolConstructor;
