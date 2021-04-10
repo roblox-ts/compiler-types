4b0a771cff293aac80663e26cdc0d0bd20a4551f
osyrisrblx <osyrisrblx@gmail.com>
2021-04-09 18:52:55 -0700

    Add intrinsic string manipulation types

--- types/core.d.ts
+++ types/core.d.ts
@@ -111,6 +111,18 @@ interface NewableFunction extends Function {}
 /** unknown - undefined = defined */
 type defined = {};
 
+/** Convert string literal type to uppercase */
+type Uppercase<S extends string> = intrinsic;
+
+/** Convert string literal type to lowercase */
+type Lowercase<S extends string> = intrinsic;
+
+/** Convert first character of string literal type to uppercase */
+type Capitalize<S extends string> = intrinsic;
+
+/** Convert first character of string literal type to lowercase */
+type Uncapitalize<S extends string> = intrinsic;
+
 /** Marker for contextual 'this' type */
 interface ThisType<T> {}
 
