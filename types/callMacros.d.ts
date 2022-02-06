527fb31eca57b868cf655abde24bf7af379b0b94
osyrisrblx <osyrisrblx@gmail.com>
2022-02-06 00:02:00 -0800

    Add `$` prefix to existing macros

--- types/callMacros.d.ts
+++ types/callMacros.d.ts
@@ -8,7 +8,7 @@ declare function assert<T>(condition: T, message?: string): asserts condition;
  * Returns the type of the given object as a string. This function works similarly to Lua’s native type function, with
  * the exceptions that Roblox-defined data types like Vector3 and CFrame return their respective data types as strings.
  */
-declare function typeOf(value: any): keyof CheckableTypes;
+declare function $typeof(value: any): keyof CheckableTypes;
 
 /**
  * Returns true if `typeof(value) == type`, otherwise false.
@@ -21,14 +21,14 @@ declare function typeOf(value: any): keyof CheckableTypes;
  * }
  * ```
  */
-declare function typeIs<T extends keyof CheckableTypes>(value: any, type: T): value is CheckableTypes[T];
+declare function $typeIs<T extends keyof CheckableTypes>(value: any, type: T): value is CheckableTypes[T];
 
 /**
  * Calls the function func with the given arguments in protected mode.
  *
  * opcall is an easier to use version of pcall. It returns a result object instead of multiple returns.
  */
-declare function opcall<T extends Array<any>, U>(
+declare function $opcall<T extends Array<any>, U>(
 	func: (...args: T) => U,
 	...args: T
 ): { success: true; value: U } | { success: false; error: string };
@@ -36,7 +36,7 @@ declare function opcall<T extends Array<any>, U>(
 /**
  * Returns true if `instance.ClassName == className`, otherwise false.
  */
-declare function classIs<T extends keyof Instances>(instance: Instance, className: T): instance is Instances[T];
+declare function $classIs<T extends keyof Instances>(instance: Instance, className: T): instance is Instances[T];
 
 /**
  * Returns the passed argument. This function is a macro that compiles to just `arg`.
@@ -48,4 +48,4 @@ declare function classIs<T extends keyof Instances>(instance: Instance, classNam
  *   pos: identity<P>({ x: 5, y: 10 });
  * }
  */
-declare function identity<T>(arg: T): T;
+declare function $identity<T>(arg: T): T;
