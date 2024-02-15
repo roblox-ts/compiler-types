f14d4aea05cca9a94a53e9cf342163d3dfa5e5cd
osyrisrblx <osyrisrblx@gmail.com>
2024-02-14 19:01:43 -0500

    Add Symbol types

--- types/Symbol.d.ts
+++ types/Symbol.d.ts
@@ -10,10 +10,37 @@ interface Symbol {
 	 * @deprecated
 	 */
 	readonly _nominal_Symbol: unique symbol;
+
+	description?: string | number;
 }
 
 interface SymbolConstructor {
-	readonly iterator: symbol;
-	readonly asyncIterator: symbol;
+	/**
+	 * Returns a new unique Symbol value.
+	 * @param  description Description of the new Symbol object.
+	 */
+	(description?: string | number): symbol;
+
+	/**
+	 * A method that returns the default iterator for an object. Called by the semantics of the
+	 * for-of statement.
+	 */
+	readonly iterator: unique symbol;
+
+	/**
+	 * A method that returns the default async iterator for an object. Called by the semantics of
+	 * the for-await-of statement.
+	 */
+	readonly asyncIterator: unique symbol;
+
+	/**
+	 * A method that is used to release resources held by an object. Called by the semantics of the `using` statement.
+	 */
+	readonly dispose: unique symbol;
+
+	/**
+	 * A method that is used to asynchronously release resources held by an object. Called by the semantics of the `await using` statement.
+	 */
+	readonly asyncDispose: unique symbol;
 }
 declare const Symbol: SymbolConstructor;
