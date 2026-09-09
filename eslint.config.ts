62f3584117dffec149c9589ca9251f151faade87
Osyris <osyrisrblx@gmail.com>
2026-09-09 10:46:29 -0400

    Use TypeScript ESLint configuration

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,12 @@
-const { fixupPluginRules } = require("@eslint/compat");
-const js = require("@eslint/js");
-const noAutofix = require("eslint-plugin-no-autofix");
-const prettierRecommended = require("eslint-plugin-prettier/recommended");
-const simpleImportSort = require("eslint-plugin-simple-import-sort");
-const tseslint = require("typescript-eslint");
+import { fixupPluginRules } from "@eslint/compat";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import noAutofix from "eslint-plugin-no-autofix";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tseslint from "typescript-eslint";
 
-module.exports = [
+export default defineConfig(
 	{ ignores: ["types/eslintIgnore.d.ts"] },
 	js.configs.recommended,
 	...tseslint.configs.recommended,
@@ -84,4 +85,4 @@ module.exports = [
 			"@typescript-eslint/no-wrapper-object-types": "off",
 		},
 	},
-];
+);
